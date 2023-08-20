d20b29e04d635b4049baadb3313a5526453a2a4f
Lywnna <vitor.valdemarca@gmail.com>
2023-08-19 18:12:27 -0700

    adding

---
+++ shop.py
@@ -0,0 +1,11 @@
+def shop():
+    lista ={
+        "!shop buy" : "Fazer suas compras",
+        "!shop ver" : "Para ver os itens disponiveis",
+        "!shop daily": "Pegue suas recompensas diarias para não ficar zerado",
+        "!inv" : "Olhe seu inventario"
+    }
+    return lista
+
+if __name__ == "__main__":
+    shop()
